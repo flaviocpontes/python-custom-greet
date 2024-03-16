0a01139b274aa93444b0de5067d5ae34e64ac9b0
Flavio Pontes <flaviocpontes@gmail.com>
2024-03-16 20:20:29 -0300

    Release 1.0.0

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 from os import getenv
 
+_VERSION = '1.0.0'
+
 greetings = getenv('GREET', 'Hello')
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def greet():
     return jsonify({'message': greetings})
